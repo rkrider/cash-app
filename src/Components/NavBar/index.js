28c38beba54e1cec5fe4e78e8decec0ecf30c8ce
mohit355 <mohitraj772002@gmail.com>
2023-08-26 21:02:18 +0530

    commit

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Logo from "../../assets/logo.png"
 import Eye from "../../assets/eye button.svg"
+import Menu from "../../assets/menu.png"
 import "./style.css"
+import { useState } from 'react'
 
 const NavBar = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleMenuIconClick=()=>{
+    setIsMobile(isMobile=>!isMobile);
+  }
+
   return (
     <div className='nav'>
         <img src={Logo} alt='logo icon'/>
-        <div className='nav-part2'>
+        <div className='wrapper'>
+        <div className={`nav-part2 ${isMobile?'nav-part2-mobile':''} `}>
             <h4 className='nav-items'>SIGN IN</h4>
             <h4 className='nav-items'>LEGAL</h4>
             <h4 className='nav-items'>LICENSE</h4>
@@ -18,7 +28,9 @@ const NavBar = () => {
             <h4 className='nav-items'>STATUS</h4>
             <h4 className='nav-items'>CODE BLOGS</h4>
         </div>
+        </div>
         <img className='eye' src={Eye} alt='eye icon'/>
+        <img className='menu' onClick={handleMenuIconClick} src={Menu} alt='Menu icon'/>
     </div>
   )
 }
