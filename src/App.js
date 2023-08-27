32924ae43d9fc46fd412ecd34a7e769dff31b3cb
Bumblebee <rahulomnitrics@gmail.com>
2023-08-27 11:35:36 +0530

    payment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import MainLayer from "./Components/MainLayer/index";
 import Payments from './Components/Payments';
 
+import './App.css';
+
 function App() {
   return (
     <main>
        <MainLayer/>
        <Payments/>
+       <section className="section3">
+       </section>
     </main>
   );
 }
--- src/Components/Payments/index.js
+++ src/Components/Payments/index.js
@@ -1,32 +1,79 @@
-import React from 'react'
-import BackgroundLining from "../../assets/payments/background-lining.png";
+import React from "react";
 import PaymentColumn from "../../assets/payments/payments-column.png";
 import PaymentPhone from "../../assets/payments/payments-phone 1.png";
 import PaymentPillerLarge from "../../assets/payments/payments-pillar-large.png";
 import PaymentPillerMedium from "../../assets/payments/payments-pillar-medium.png";
 import PaymentPillerSmall from "../../assets/payments/payments-pillar-small.png";
 
-
 import "./style.css";
 
 const Payments = () => {
   return (
-    <div className='payments'>
-        <div className='payments_main'>
-        <div className='payments_bg'>
-            <img className='backgroundLining' src={BackgroundLining} alt='backgroundLining'/>
+    <section className="payments">
+      <div className="payment_upper">
+        <div className="text">
+          <h1>Payments</h1>
+          <p>
+            Send and receive money with anyone, donate to an important cause, or
+            tip professionals. Just enter a $cashtag, phone number, or scan
+            their QR code to pay.
+          </p>
         </div>
-        <div className='payments_imgs'>
-            <img className='paymentPhone' src={PaymentPhone} alt='paymentPhone'/>
-            <img className='paymentPillersmall' src={PaymentPillerSmall} alt='paymentPillersmall'/>
-            <img className='paymentPillerMedium' src={PaymentPillerMedium} alt='paymentPillerMedium'/>
-            <img className='paymentPillerLarge' src={PaymentPillerLarge} alt='paymentPillerLarge'/>
-            <img className='paymentColumn' src={PaymentColumn} alt='paymentColumn'/>
-        </div>
-        </div>
+      </div>
+      <div className="payments_below">
+        <div className="payments_bg"></div>
+        <div className="payments_imgs">
+          <img
+            className="paymentColumn1"
+            src={PaymentColumn}
+            alt="paymentColumn"
+          />
+          <img className="paymentPhone" src={PaymentPhone} alt="paymentPhone" />
+          <img
+            className="paymentPillerMedium1"
+            src={PaymentPillerMedium}
+            alt="paymentPillerMedium"
+          />
+          <img
+            className="paymentPillersmall"
+            src={PaymentPillerSmall}
+            alt="paymentPillersmall"
+          />
+          <img
+            className="paymentPillerMedium2"
+            src={PaymentPillerMedium}
+            alt="paymentPillerMedium"
+          />
+          <img
+            className="paymentPillerLarge"
+            src={PaymentPillerLarge}
+            alt="paymentPillerLarge"
+          />
+          <img
+            className="paymentPillerMedium3"
+            src={PaymentPillerMedium}
+            alt="paymentPillerMedium"
+          />
+          <img
+            className="paymentPillerMedium"
+            src={PaymentPillerMedium}
+            alt="paymentPillerMedium"
+          />
 
-    </div>
-  )
-}
+          <img
+            className="paymentColumn2"
+            src={PaymentColumn}
+            alt="paymentColumn1"
+          />
+          <img
+            className="paymentColumn3"
+            src={PaymentColumn}
+            alt="paymentColumn"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Payments
+export default Payments;
